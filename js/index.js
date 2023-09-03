df90b8a977bbf9d5926904d86b21616cfb335780
poda850 <poda850@gmail.com>
2023-09-03 23:51:55 +0300

    2nd

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-// $("body").css("overflow","hidden")
+$("body").css("overflow","hidden")
 
 $(document).ready(() => {
-  search("s","").then(() => {
-    $(".loading-screen").fadeOut(500)
+  hideSideBar()
+  search("s","")
+    $(".loading-screen").fadeOut(1000,function() {
+      $(".loading-screen").remove()
+    })
+
     $("body").css("overflow", "auto")
 
-  })
 })
 
 function hideSideBar() {
@@ -19,7 +22,7 @@ function hideSideBar() {
     $(".links").eq(i).animate({ top: "400px" })
   }
 }
-hideSideBar()
+
 
 
 function showSideBar() {
@@ -45,7 +48,6 @@ $(".navIcon").click(() => {
 //............................................................................................................
 
 $("#searchInputs").hide();
-
 $("#search").click(() => {
   hideSideBar();
   $("#searchInputs").show();
@@ -122,18 +124,16 @@ function getMealIngredient(array) {
 
 
 function getMealTags(array) {
-  let lis = "";
-  let tagsArr = array[0].strTags.split(",")
-  for (let i = 0; i < tagsArr.length; i++) {
-    
-    if (array[0].strTags) {
-      lis += `
-      <li class="bg-danger-subtle p-2 rounded-3 m-2 text-danger">${tagsArr[i]}</li>
-        `
-    }
 
+  let tags = array[0].strTags?.split(",")
+  if (!tags) tags = []
+
+  let tagsStr = ''
+  for (let i = 0; i < tags.length; i++) {
+    tagsStr += `
+        <li class="alert alert-danger m-2 p-1">${tags[i]}</li>`
   }
-  $(".tagList").html(lis)
+   $(".tagList").html(tagsStr)
 }
 
 
@@ -233,6 +233,7 @@ async function displayCategoryMeals(strCategory) {
 }
 
 //............................................................................................................
+
 $("#area").click(function () { 
   $("#searchInputs, #mealDetails, .api-data").html("")
   hideSideBar();
@@ -243,7 +244,6 @@ $("#area").click(function () {
 async function getArea() {
   let apiResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
   let finalJson = await apiResponse.json();
-  console.log(finalJson.meals);
   displayArea(finalJson.meals)
 }
 
@@ -269,16 +269,228 @@ async function displayAreaMeals(strArea) {
   displayMeals(finalJson.meals)
 }
 
-
-// async function getAreaMeals(area) {
-//   rowData.innerHTML = ""
-//   $(".inner-loading-screen").fadeIn(300)
-
-//   let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
-//   response = await response.json()
+//............................................................................................................
 
 
-//   displayMeals(response.meals.slice(0, 20))
-//   $(".inner-loading-screen").fadeOut(300)
+$("#ingredients").click(function () { 
 
-// }
+  $("#searchInputs, #mealDetails, .api-data").html("")
+  hideSideBar();
+  getIngredients();
+  
+});
+
+async function getIngredients() {
+  let apiResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`);
+  let finalJson = await apiResponse.json();
+  console.log(finalJson.meals.slice(0, 20));
+  displayIngredients(finalJson.meals.slice(0, 20))
+}
+
+
+function displayIngredients(array) {
+  let cartoona = "";
+
+  for (let i = 0; i < array.length; i++) {
+    cartoona += `
+    <div class="col-md-3">
+      <div onclick="displayIngredientsMeal('${array[i].strIngredient}')" class="rounded-2 text-center cursor-pointer text-light">
+              <i class="fa-solid fa-drumstick-bite fa-4x"></i>
+              <h3>${array[i].strIngredient}</h3>
+              <p>${array[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
+      </div>
+    </div>
+      `
+  }
+  $(".api-data").html(cartoona)
+}
+
+async function displayIngredientsMeal(ingredients) {
+  let apiResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`);
+  let finalJson = await apiResponse.json();
+  displayMeals(finalJson.meals)
+}
+
+//............................................................................................................
+$("#contact").click(function () { 
+
+  $("#searchInputs, #mealDetails, .api-data").html("")
+  hideSideBar();
+  showContacts();
+  
+});
+
+
+function showContacts() {
+  $("main").html(`<div class="contact min-vh-100 d-flex justify-content-center align-items-center">
+    <div class="container w-75 text-center">
+        <div class="row g-4">
+            <div class="col-md-6">
+                <input id="nameInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Name">
+                <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Special characters and numbers not allowed
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="emailInput" onkeyup="inputsValidation()" type="email" class="form-control " placeholder="Enter Your Email">
+                <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Email not valid *exemple@yyy.zzz
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="phoneInput" onkeyup="inputsValidation()" type="text" class="form-control " placeholder="Enter Your Phone">
+                <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid Phone Number
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="ageInput" onkeyup="inputsValidation()" type="number" class="form-control " placeholder="Enter Your Age">
+                <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid age
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="passwordInput" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Enter Your Password">
+                <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid password *Minimum eight characters, at least one letter and one number:*
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="repasswordInput" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Repassword">
+                <div id="repasswordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid repassword 
+                </div>
+            </div>
+        </div>
+        <button id="submitBtn" disabled class="btn btn-outline-danger px-2 mt-3">Submit</button>
+    </div>
+</div> `)
+  submitBtn = document.getElementById("submitBtn")
+
+
+  document.getElementById("nameInput").addEventListener("focus", () => {
+    nameInputTouched = true
+  })
+
+  document.getElementById("emailInput").addEventListener("focus", () => {
+    emailInputTouched = true
+  })
+
+  document.getElementById("phoneInput").addEventListener("focus", () => {
+    phoneInputTouched = true
+  })
+
+  document.getElementById("ageInput").addEventListener("focus", () => {
+    ageInputTouched = true
+  })
+
+  document.getElementById("passwordInput").addEventListener("focus", () => {
+    passwordInputTouched = true
+  })
+
+  document.getElementById("repasswordInput").addEventListener("focus", () => {
+    repasswordInputTouched = true
+  })
+}
+
+let nameInputTouched = false;
+let emailInputTouched = false;
+let phoneInputTouched = false;
+let ageInputTouched = false;
+let passwordInputTouched = false;
+let repasswordInputTouched = false;
+
+
+
+
+function inputsValidation() {
+  if (nameInputTouched) {
+    if (nameValidation()) {
+      document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+
+    } else {
+      document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+  if (emailInputTouched) {
+
+    if (emailValidation()) {
+      document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+    } else {
+      document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+
+  if (phoneInputTouched) {
+    if (phoneValidation()) {
+      document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+    } else {
+      document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+
+  if (ageInputTouched) {
+    if (ageValidation()) {
+      document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+    } else {
+      document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+
+  if (passwordInputTouched) {
+    if (passwordValidation()) {
+      document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+    } else {
+      document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+  if (repasswordInputTouched) {
+    if (repasswordValidation()) {
+      document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+    } else {
+      document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+
+    }
+  }
+
+
+  if (nameValidation() &&
+    emailValidation() &&
+    phoneValidation() &&
+    ageValidation() &&
+    passwordValidation() &&
+    repasswordValidation()) {
+    submitBtn.removeAttribute("disabled")
+  } else {
+    submitBtn.setAttribute("disabled", true)
+  }
+}
+
+function nameValidation() {
+  return (/^[a-zA-Z ]+$/.test(document.getElementById("nameInput").value))
+}
+
+function emailValidation() {
+  return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("emailInput").value))
+}
+
+function phoneValidation() {
+  return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("phoneInput").value))
+}
+
+function ageValidation() {
+  return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("ageInput").value))
+}
+
+function passwordValidation() {
+  return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("passwordInput").value))
+}
+
+function repasswordValidation() {
+  return document.getElementById("repasswordInput").value == document.getElementById("passwordInput").value
+}
